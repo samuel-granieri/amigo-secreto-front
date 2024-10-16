d46dab48a6870f498c26880ca38e1c256c72889b
Samuel Granieri Martins - PrestServ <samuel.granieri.prestserv@petrobras.com.br>
2024-10-16 16:05:27 -0300

    push alteracoes front

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ if(form){
    
       
         if (!editMode){
+            e.preventDefault();
 
             data = {
                 nome: nome,
@@ -31,6 +32,8 @@ if(form){
                 calca: calca,
                 observacoes: observacoes
             }
+
+
 
         
             const response = await fetch(`https://amigo-secreto-back-h9n4.onrender.com
@@ -43,10 +46,11 @@ if(form){
                 body: JSON.stringify(data) // Convertendo os dados para JSON
             });
             alert('Registro salvo com sucesso!')
+            window.location.reload()
             return await response.json(); // Convertendo a resposta em JSON
         }
         else{
-
+            e.preventDefault()
             data = {
                 _id: id,
                 nome: nome,
@@ -66,12 +70,15 @@ if(form){
                 body: JSON.stringify(data) // Convertendo os dados para JSON
             });
             alert('Registro editado com sucesso!')
+            window.location.reload()
             editMode = true
             return await response.json(); // Convertendo a resposta em JSON
             
         }
-
+        
+    
     })
+
 }
 
 
@@ -114,6 +121,13 @@ const fetchAndLogUsers = async () => {
 </svg> 
                          
                         </td>
+                        <td class="text-center opcao-editar" onclick="handleDelete('${element._id}')">
+
+                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+  <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47M8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+</svg>
+
+                        </td>
 
                     </tr>
                 `
@@ -130,6 +144,7 @@ fetchAndLogUsers();
 
 //opcao editar
 const handleEdit = (id_user, nome, sapato, blusa, calca, observacoes)=> {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
 
     document.getElementById('nome').value = nome;
     document.getElementById('sapato').value = sapato;
@@ -139,6 +154,29 @@ const handleEdit = (id_user, nome, sapato, blusa, calca, observacoes)=> {
 
     editMode = true
     id = id_user
+}
+
+const handleDelete = async(id_user)=> {
+    data = {
+        _id: id_user
+    }
+    
+    const response = await fetch(`https://amigo-secreto-back-h9n4.onrender.com
+
+        /deleteUser`,
+        // const response = await fetch(`http://localhost:3000/deleteUser`, 
+        
+        {
+            method: 'POST', // Definindo o método como POST
+            headers: {
+              'Content-Type': 'application/json' // Tipo do conteúdo enviado
+            },
+            body: JSON.stringify(data) // Convertendo os dados para JSON
+        });
+                    //alert('Registro deletado com sucesso!')
+    window.location.reload()
+    return await response.json() // Convertendo a resposta em JSON
+    
 }
 
 
